0530d0720f28eacd8a7226c7f52160332ea26a2c
Serge Pille <serge@pille.be>
2020-09-03 14:58:36 +0200

    Add project files.

---
+++ VideoStore/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoStore.Models;
+
+namespace VideoStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int? Id)
+        {
+            Customer customer = GetCustomers().FirstOrDefault(c => c.Id == Id);
+
+            if (customer == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name="John Smith"},
+                new Customer {Id = 2, Name="Mary Williams"},
+                new Customer {Id = 3, Name="Vicky Engels"},
+                new Customer {Id = 4, Name="Idris Elba"},
+                new Customer {Id = 5, Name="John Singleton"},
+            };
+
+        }
+
+    }
+}
---
+++ VideoStore/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoStore.Models;
+
+
+namespace VideoStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Details(int? Id)
+        {
+            Movie movie = GetMovies().FirstOrDefault(m => m.Id == Id);
+            if (movie == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            else
+            {
+                return View(movie);
+            }
+        }
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            //return new List<Movie>();
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name= "Heat"},
+                new Movie {Id = 2, Name= "First Blood"},
+                new Movie {Id = 3, Name= "Parasite"},
+                new Movie {Id = 4, Name= "The Invisible Man"},
+                new Movie {Id = 5, Name= "Gladiator"},
+            };
+        }
+
+    }
+}
