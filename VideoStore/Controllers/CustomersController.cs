028a038d329cb010d82e719165909686f0f16324
Serge Pille <serge@pille.be>
2020-09-03 16:51:03 +0200

    CustomerAnnotations

--- VideoStore/Controllers/CustomersController.cs
+++ VideoStore/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,17 +10,32 @@ namespace VideoStore.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();    
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
             return View(customers);
         }
 
 
         public ActionResult Details(int? Id)
         {
-            Customer customer = GetCustomers().FirstOrDefault(c => c.Id == Id);
+            //Customer customer = GetCustomers().FirstOrDefault(c => c.Id == Id); Hardcoded list
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
 
             if (customer == null)
             {
@@ -32,18 +48,18 @@ namespace VideoStore.Controllers
         }
 
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name="John Smith"},
-                new Customer {Id = 2, Name="Mary Williams"},
-                new Customer {Id = 3, Name="Vicky Engels"},
-                new Customer {Id = 4, Name="Idris Elba"},
-                new Customer {Id = 5, Name="John Singleton"},
-            };
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Id = 1, Name="John Smith"},
+        //        new Customer {Id = 2, Name="Mary Williams"},
+        //        new Customer {Id = 3, Name="Vicky Engels"},
+        //        new Customer {Id = 4, Name="Idris Elba"},
+        //        new Customer {Id = 5, Name="John Singleton"},
+        //    };
 
-        }
+        //}
 
     }
 }
