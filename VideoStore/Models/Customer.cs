3ba0763dfccd112ff48fd1b5f95dac46c655f4c3
Serge Pille <serge@pille.be>
2020-09-03 15:44:18 +0200

    MembershipTypes

--- VideoStore/Models/Customer.cs
+++ VideoStore/Models/Customer.cs
@@ -9,6 +9,10 @@ namespace VideoStore.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType membershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
 
     }
 }
