8f74a16cc5ce1a2fafbdda077ec9853bd7c91d04
Serge Pille <serge@pille.be>
2020-09-06 18:42:19 +0200

    Excercise4

--- VideoStore/Controllers/CustomersController.cs
+++ VideoStore/Controllers/CustomersController.cs
@@ -51,17 +51,50 @@ namespace VideoStore.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer )
+        public ActionResult Save(Customer customer )
         {
-            return View();
+            if (customer.Id==0)
+            {
+                _context.Customers.Add(customer);
+
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+        public ActionResult Edit(int Id)
+        {
+            Customer customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+            if (customer==null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            
+            return View("CustomerForm", viewModel);
         }
 
 
