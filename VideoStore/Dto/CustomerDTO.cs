900f8ab5a4741c8ce389979eb39b4cf813b62573
Serge Pille <serge@pille.be>
2020-09-12 17:33:58 +0200

    CustomerDTO

---
+++ VideoStore/Dto/CustomerDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VideoStore.Models;
+
+namespace VideoStore.Dto
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthday { get; set; }
+
+    }
+}
