8096de4e4e72648182a578d42f2206219a84d0f1
Serge Pille <serge@pille.be>
2020-09-13 16:58:30 +0200

    IHttpActionResult

--- VideoStore/Dto/CustomerDTO.cs
+++ VideoStore/Dto/CustomerDTO.cs
@@ -19,7 +19,7 @@ namespace VideoStore.Dto
 
         public byte MembershipTypeId { get; set; }
 
-        [Min18YearsIfAMember]
+        //[Min18YearsIfAMember]
         public DateTime? Birthday { get; set; }
 
     }
